19431e3653e92b24460e9122c60a9cbda9d0edb7
sandeep <seopoint35@gmail.com>
2021-03-23 00:43:47 -0400

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Login from "./Components/Login/Login";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
 
 function App () {
   return (
      <>
      <BrowserRouter>
+     <Route exact  path="/" component={Home} />
      <Route exact  path="/Login" component={Login} />
      </BrowserRouter>
   
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../../assets/css/NavBar.css';
+import Logo from '../../assets/images/logo.png';
+import { NavLink } from "react-router-dom";
+
+
+
+export default function NavBar() {
+    return (
+        <>
+            <div className="NavBarContainer">
+
+                <div className="MobileIcon">
+                <i class="fas fa-bars"></i>
+                </div>
+
+                <div className="LogoBox">
+                    <img src={Logo} />
+                </div>
+
+                <div className="forSpace">
+
+                </div>
+
+                <div className="Notification">
+                    <div className="NotificationIcon">
+                        <i class="fas fa-bell"></i>
+                    </div>
+                    <div className="LoginButton">
+                        <NavLink to="/Login"> Login</NavLink>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </>
+    )
+}
